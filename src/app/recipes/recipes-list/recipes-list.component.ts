4b431b09ccfa5ebcee6b7559ef11c4d68fc442ea
anh.phan <anh.phan@sutrixsolutions.com>
2018-07-20 19:02:50 +0700

    First commit

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simply recipe', 'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1506120378/MR_0917170472.jpg?itok=KPTNrvis'),
+    new Recipe('A test recipe', 'This is simply recipe', 'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1506120378/MR_0917170472.jpg?itok=KPTNrvis')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
